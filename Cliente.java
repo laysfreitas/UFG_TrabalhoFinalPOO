60a376ebddda0b2d9552c6bc854239e8c7fb9cbb
IngridPs <71459442+IngridPs@users.noreply.github.com>
2021-05-27 07:12:43 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,11 +1,24 @@
 import java.util.ArrayList;
 
-public ArrayList<Compra>compras = new ArrayList<Compra>();
+public ArrayList<Compra> compras;
 
-	public Funcionario(String nome, String cpf, Endereço endereço, String telefone) {
-		super(nome, cpf, endereço, telefone);
-	}
-
-public void realizaCompra(Compra compra){
-    compras.add(compra);  
-}
+    public void realizaCompra(Produto produto){
+        
+    }
+    public boolean efetuaPagamento(Compra compra){
+    
+    }
+    public void solicitaTroca(Produto p1, Produto p2){
+        p1.quantidade++;
+        p2.quantidade--;
+        int i,j;
+        for(i=0; i<compras.size();i++){
+            for(j=0; j<compras.get(i).produtos.size();i++){
+                if(p1.codigo == (compras.get(i).produtos.get(j)).codigo){
+                    compras.get(i).produtos.get(j).remove();
+                    p1++;
+                    realizaCompra(p2);
+                }
+            }
+        }
+    }
